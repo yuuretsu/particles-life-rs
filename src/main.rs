093cf8bfe04a28ee78088cd27ff9406720fd8520
Yura Frozen <yurafrozen@gmail.com>
2022-10-19 22:07:46 +0500

    draggable

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use macroquad::prelude::*;
 
 use particles_life::*;
 
-fn update_image(particles: &Particles, colors: &[Color; PARTICLES_TYPES_AMOUNT]) {
-    let dx = screen_width() / 2.;
-    let dy = screen_height() / 2.;
+fn update_image(particles: &Particles, colors: &[Color; PARTICLES_TYPES_AMOUNT], dx: f32, dy: f32) {
+    let dx = screen_width() / 2. + dx;
+    let dy = screen_height() / 2. + dy;
     clear_background(BLACK);
     for p in particles.list() {
         let mut color = colors[p.t as usize];
@@ -28,7 +28,14 @@ async fn main() {
 
     let mut paused = false;
 
+    let mut is_dragging = false;
+    let mut dx = 0.0;
+    let mut dy = 0.0;
+    let mut offset_x = 0.0;
+    let mut offset_y = 0.0;
+
     loop {
+        let (mouse_x, mouse_y) = mouse_position();
         if !paused {
             particles.step(&mut rng, &rules);
         }
@@ -53,7 +60,22 @@ async fn main() {
             });
         });
 
-        update_image(&particles, &colors);
+        if is_dragging {
+            dx = mouse_x + offset_x;
+            dy = mouse_y + offset_y;
+        }
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            is_dragging = true;
+            offset_x = dx - mouse_x;
+            offset_y = dy - mouse_y;
+        }
+
+        if is_mouse_button_released(MouseButton::Left) {
+            is_dragging = false;
+        }
+
+        update_image(&particles, &colors, dx, dy);
         egui_macroquad::draw();
 
         next_frame().await
